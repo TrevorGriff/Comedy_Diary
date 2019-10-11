7a0228eb148abb18f599277cc29f18faae19d258
Trev <trevg@aol.com>
2019-10-11 12:03:54 -0700

    initial implementation of set list

--- Comedy_Diary/Controllers/SetListController.swift
+++ Comedy_Diary/Controllers/SetListController.swift
@@ -1,5 +1,5 @@
 //
-//  SetLisrController.swift
+//  SetListController.swift
 //  ComedyOrganizer
 //
 //  Created by Trevor Griffiths on 9/6/19.
@@ -8,11 +8,45 @@
 
 import Foundation
 import UIKit
+import RealmSwift
+
+class SetListRow: UITableViewCell{
+    
+    
+    
+}
 
 class SetListController : UIViewController {
+
+    @IBOutlet weak var setTable: UITableView!
+    
+    let realm = try! Realm()
+    
+    lazy var sets = realm.objects(ASet.self)
     
     override func viewDidLoad(){
         super.viewDidLoad()
+        
+        setTable?.delegate = self as! UITableViewDelegate
+        
+        setTable?.dataSource = self as! UITableViewDataSource
+        
+        loadSets()
+        
+    }
+    
+    func loadSets(){
+        
+        sets = realm.objects(ASet.self)
+        
+        setTable.reloadData()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool){
+        
+        self.setTable?.reloadData()
+        
     }
     
     @IBAction func homeButton(_ sender: Any) {
@@ -20,5 +54,26 @@ class SetListController : UIViewController {
         dismiss(animated: true, completion: nil)
         
     }
+    
+}
+
+
+extension SetListController: UITableViewDataSource, UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        
+        return sets.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReuseRow", for: indexPath) as! SetListRow
+        
+        
+        
+        return cell
+    }
+    
     
 }
